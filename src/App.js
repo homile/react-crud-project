5dc94a0e57fe375e624a6d85414f91e63a480350
homile <ssanho2@naver.com>
2022-06-14 21:08:27 +0900

    create-react-app 생성 1.1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './App.css'
 
 function App() {
   return (
-    <div>App</div>
+    <header className='App-header'>React-CRUD-Project</header>
   )
 }
 
---
+++ src/resource/dummyData.js
@@ -0,0 +1,25 @@
+const dummy = [
+  {
+    id: 3,
+    username: 'SooBin',
+    content:
+      '킹받네~!',
+    createdAt: '2022-02-26T16:17:47.000Z',
+  },
+  {
+    id: 2,
+    username: 'YoungWoong',
+    content:
+      '똑빈님 안녕하세요',
+    createdAt: '2022-02-25T16:17:47.000Z',
+  },
+  {
+    id: 1,
+    username: 'MinWoo',
+    content:
+      '이 프로젝트 성공 할 수 있습니다. 여러분!!! 저도 열심히 할게요!!',
+    createdAt: '2022-02-24T16:17:47.000Z',
+  },
+];
+
+export default dummy;
